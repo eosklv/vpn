72d52750fee24fa10e9c3cec9e2bc004441077fa
Eugeny Sokolov <eugeny.sokolov@etna.etnasoft.com>
2024-03-19 19:41:48 +0500

    Fixed bugs

--- scripts/server_init.sh
+++ scripts/server_init.sh
@@ -5,4 +5,4 @@ sudo apt install openvpn easy-rsa unzip -y
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o ~/"awscliv2.zip"
 unzip ~/awscliv2.zip -d ~/
-sudo ./aws/install
+sudo ~/aws/install
