cedfff9fdffbeb762343feb555a53323791673b2
Eugeny Sokolov <eugeny.sokolov@etna.etnasoft.com>
2024-03-20 22:57:55 +0500

    Added Github actions files

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,12 +88,12 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
   to_port           = 22
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_udp_on_443_ipv4" {
+resource "aws_vpc_security_group_ingress_rule" "allow_tcp_on_443_ipv4" {
   security_group_id = aws_security_group.allow_connecting.id
   for_each          = toset(var.my_ips)
   cidr_ipv4         = each.key
   from_port         = 443
-  ip_protocol       = "udp"
+  ip_protocol       = "tcp"
   to_port           = 443
 }
 
