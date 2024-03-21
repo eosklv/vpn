47d82c5dc36002fa551bc316f4e4f8b834f64a84
Eugeny Sokolov <eugeny.sokolov@etna.etnasoft.com>
2024-03-21 22:49:10 +0500

    Added telegram bot code

---
+++ telegram/main.py
@@ -0,0 +1,36 @@
+import telebot
+import subprocess
+import os
+
+token = "6997976277:AAGdpGgxgYNpwCxQYx0PBu_40HLPes61EGo"
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    check = message.text.lower().replace('!', '')
+    if check in ['hi', 'hello']:
+        bot.send_message(message.chat.id, 'Long time no see, my dear!')
+
+    elif check in ['bye', 'good bye', 'see you']:
+        bot.send_message(message.chat.id, 'Talk to you soon!')
+
+    elif check in ['start', 'run', 'go']:
+        bot.send_message(message.chat.id, "Here we go... Hold on a second...")
+        result = subprocess.run('terraform -chdir=../terraform/ apply -auto-approve', shell=True, capture_output=True)
+        bot.send_message(message.chat.id, result.stdout.splitlines()[-5].decode("utf-8"))
+        bot.send_message(message.chat.id, result.stdout.splitlines()[-1].decode("utf-8")[3:])
+        # img = open('dict.txt', 'rb')
+        # bot.send_photo(message.chat.id, img)
+
+    elif check in ['stop', 'end', 'finish']:
+        bot.send_message(message.chat.id, "OK, I'll do my best, but can't promise... Hold on a second...")
+        result = subprocess.run('terraform -chdir=../terraform/ destroy -auto-approve', shell=True, capture_output=True)
+        bot.send_message(message.chat.id, "Before leaving me, she said:")
+        bot.send_message(message.chat.id, result.stdout.splitlines()[-2].decode("utf-8"))
+
+    else:
+        bot.send_message(message.chat.id, 'Thank you... Thank you for being so dumb!')
+
+
+bot.polling()
