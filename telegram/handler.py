271bfa28a9447ccadd68b3d0cedfacabbb6378ad
Eugeny Sokolov <eugeny.sokolov@etna.etnasoft.com>
2024-03-23 15:40:44 +0500

    Updated project structure

--- telegram/handler.py
+++ telegram/handler.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 here = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.join(here, "./vendored"))
+sys.path.append(os.path.join(here, "./.vendored"))
 
 import requests
 
