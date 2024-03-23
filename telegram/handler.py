cc02bcf770100c0ad8ef78a18c24a79bc6209c9d
Eugeny Sokolov <eugeny.sokolov@etna.etnasoft.com>
2024-03-23 19:35:32 +0500

    Updated bot logic

--- telegram/handler.py
+++ telegram/handler.py
@@ -14,8 +14,10 @@ BASE_URL = "https://api.telegram.org/bot{}".format(TOKEN)
 s3_client = boto3.client('s3')
 
 
-def send_message(chat_id, response):
+def send_message(chat_id, response, parse_mode = False):
     data = {"text": response.encode("utf8"), "chat_id": chat_id}
+    if parse_mode:
+        data["parse_mode"] = parse_mode
     url = BASE_URL + "/sendMessage"
     requests.post(url, data)
 
@@ -26,7 +28,7 @@ def handler(event, context):
         chat_id = data["message"]["chat"]["id"]
         first_name = data["message"]["chat"]["first_name"]
 
-        if "start" in message:
+        if "start" in message or "hi" in message or "hello" in message or "hey" in message:
             send_message(chat_id, f"Long time no see, {first_name}!")
 
         elif "how are you" in message:
@@ -35,8 +37,11 @@ def handler(event, context):
         elif "run" in message:
             send_message(chat_id, "Here we go... Hold on a moment...")  
             s = s3_client.generate_presigned_url('get_object', Params = {'Bucket': 'esklv-vpn', 'Key': 'profiles/client.ovpn'}, ExpiresIn = 300)
-            send_message(chat_id, f"Your VPN profile is available by <a href={s}>this</a> link.")
+            send_message(chat_id, f"Your VPN profile is available by [this]({s}) link", "MarkdownV2")
             send_message(chat_id, "Bear in mind that this link is expiring in 5 minutes.")
+
+        elif "thanks" in message or "thank you" in message:
+            send_message(chat_id, "I know you’d do the same for me.")
 
         elif "destroy" in message.lower():
             send_message(chat_id, "I'll try my best, but can't promise... Hold on a moment...")
