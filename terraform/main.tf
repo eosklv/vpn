4cf291681c1990e728aea05721fbbcd920321e09
Eugeny Sokolov <98689193+esklv@users.noreply.github.com>
2024-11-10 17:18:07 +0500

    Updated security groups

--- terraform/main.tf
+++ terraform/main.tf
@@ -76,7 +76,7 @@ resource "aws_security_group" "allow_connecting" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tcp_on_443_ipv4" {
   security_group_id = aws_security_group.allow_connecting.id
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 443
   ip_protocol       = "udp"
   to_port           = 443
@@ -84,7 +84,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tcp_on_443_ipv4" {
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.allow_connecting.id
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
 }
 
